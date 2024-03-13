9db5d31e21489e540f7e56e1aa4646f2c6524898
thz-p <872846303@QQ.COM>
2024-03-13 02:30:29 +0000

    t

--- Assets/FancyScrollView/Sources/Runtime/Core/FancyCell.cs
+++ Assets/FancyScrollView/Sources/Runtime/Core/FancyCell.cs
@@ -9,57 +9,56 @@ using UnityEngine;
 namespace FancyScrollView
 {
     /// <summary>
-    /// <see cref="FancyScrollView{TItemData, TContext}"/> のセルを実装するための抽象基底クラス.
-    /// <see cref="FancyCell{TItemData, TContext}.Context"/> が不要な場合は
-    /// 代わりに <see cref="FancyCell{TItemData}"/> を使用します.
+    /// 用于实现 <see cref="FancyScrollView{TItemData, TContext}"/> 的单元格的抽象基类.
+    /// 如果不需要 <see cref="FancyCell{TItemData, TContext}.Context"/>，则使用 <see cref="FancyCell{TItemData}"/> 代替.
     /// </summary>
-    /// <typeparam name="TItemData">アイテムのデータ型.</typeparam>
-    /// <typeparam name="TContext"><see cref="Context"/> の型.</typeparam>
+    /// <typeparam name="TItemData">项目数据的类型.</typeparam>
+    /// <typeparam name="TContext"><see cref="Context"/> 的类型.</typeparam>
     public abstract class FancyCell<TItemData, TContext> : MonoBehaviour where TContext : class, new()
     {
         /// <summary>
-        /// このセルで表示しているデータのインデックス.
+        /// 此单元格显示的数据的索引.
         /// </summary>
         public int Index { get; set; } = -1;
 
         /// <summary>
-        /// このセルの可視状態.
+        /// 此单元格的可见状态.
         /// </summary>
         public virtual bool IsVisible => gameObject.activeSelf;
 
         /// <summary>
-        /// <see cref="FancyScrollView{TItemData, TContext}.Context"/> の参照.
-        /// セルとスクロールビュー間で同じインスタンスが共有されます. 情報の受け渡しや状態の保持に使用します.
+        /// <see cref="FancyScrollView{TItemData, TContext}.Context"/> 的引用.
+        /// 在单元格和滚动视图之间共享相同的实例. 用于传递信息和保持状态.
         /// </summary>
         protected TContext Context { get; private set; }
 
         /// <summary>
-        /// <see cref="Context"/> をセットします.
+        /// 设置 <see cref="Context"/>.
         /// </summary>
-        /// <param name="context">コンテキスト.</param>
+        /// <param name="context">上下文.</param>
         public virtual void SetContext(TContext context) => Context = context;
 
         /// <summary>
-        /// 初期化を行います.
+        /// 执行初始化.
         /// </summary>
         public virtual void Initialize() { }
 
         /// <summary>
-        /// このセルの可視状態を設定します.
+        /// 设置此单元格的可见状态.
         /// </summary>
-        /// <param name="visible">可視状態なら <c>true</c>, 非可視状態なら <c>false</c>.</param>
+        /// <param name="visible"><c>true</c> 表示可见状态，<c>false</c> 表示不可见状态.</param>
         public virtual void SetVisible(bool visible) => gameObject.SetActive(visible);
 
         /// <summary>
-        /// アイテムデータに基づいてこのセルの表示内容を更新します.
+        /// 根据项目数据更新此单元格的显示内容.
         /// </summary>
-        /// <param name="itemData">アイテムデータ.</param>
+        /// <param name="itemData">项目数据.</param>
         public abstract void UpdateContent(TItemData itemData);
 
         /// <summary>
-        /// <c>0.0f</c> ~ <c>1.0f</c> の値に基づいてこのセルのスクロール位置を更新します.
+        /// 根据 <c>0.0f</c> ~ <c>1.0f</c> 的值更新此单元格的滚动位置.
         /// </summary>
-        /// <param name="position">ビューポート範囲の正規化されたスクロール位置.</param>
+        /// <param name="position">规范化的滚动位置，位于视口范围内.</param>
         public abstract void UpdatePosition(float position);
     }
 
